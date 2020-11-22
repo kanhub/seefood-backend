313b44efa867e90bc71fbd5cef963e4a403534e8
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-11-22 17:00:34 -0500

    refactored backend with pgsql

--- server.js
+++ server.js
@@ -1,91 +1,38 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import cors from "cors";
+import knex from "knex";
+import handleRegister from "./controllers/register.js";
+import handleSignIn from "./controllers/signin.js";
+import handleProfileGet from "./controllers/profile.js";
+import handleImage from "./controllers/image.js";
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "kanisk",
+    password: "",
+    database: "seefood",
+  },
+});
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const db = {
-  users: [
-    {
-      id: "123",
-      name: "John",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Sally",
-      email: "sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
   res.send(db.users);
 });
 
-app.post("/signin", (req, res) => {
-  if (
-    req.body.email === db.users[0].email &&
-    req.body.password === db.users[0].password
-  ) {
-    res.json(db.users[0]);
-  } else {
-    res.status(400).json("Error logging in");
-  }
-});
+app.post("/signin", handleSignIn(db, bcrypt));
 
-app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  bcrypt.hash(password, 10, (err, hash) => {
-    console.log(hash);
-  });
-  db.users.push({
-    id: "125",
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date(),
-  });
-  res.json(db.users[db.users.length - 1]);
-});
+app.post("/register", handleRegister(db, bcrypt));
 
-app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  let found = false;
-  db.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("no such user");
-  }
-});
+app.get("/profile/:id", handleProfileGet(db));
 
-app.put("/image", (req, res) => {
-  const { id } = req.body;
-  let found = false;
-  db.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("no such user");
-  }
-});
+app.put("/image", handleImage(db));
 
 app.listen(3000, () => {
   console.log("App running on port 3000");
